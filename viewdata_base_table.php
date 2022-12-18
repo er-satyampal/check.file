21b7a6f9edb06c7f67a7e7c21a728fb6fcb91717
unknown <em.satyam@gmail.com>
2022-12-18 21:39:22 +0530

    Initial Commits

---
+++ viewdata_base_table.php
@@ -0,0 +1,68 @@
+<?php include_once(__DIR__."/db/connection.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th, tr {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+  text-align: center;
+}
+</style>
+</head>
+<body>
+    <h2>DataBase-Table</h2>
+    <table>
+        <tr>
+            <th>Topic Id</th>
+            <th>Date</th>
+            <th>Topic Name</th>
+            <th>Preview link</th>
+            <th>Changes</th>
+        </tr>
+<?php
+$main = $connection->prepare("SELECT * FROM `officework`");
+$main->setFetchMode(PDO:: FETCH_OBJ);
+
+$main->execute();
+
+while($row=$main->fetch()){
+
+    $del = 'http://localhost/check.file/delete.php?uid='.$row->topic_id;
+    $edit = 'http://localhost/check.file/update.php?='.$row->topic_id;
+
+
+    echo '
+  <tr>
+    <td>'.$row->topic_id.'</td>
+    <td>'.$row->datetime. '</td>
+    <td>'.$row->topic_name.'</td>
+    <td><a href="('.$row->preview_link.')">Open</a></td>
+    <td>
+        <a href="'.$del.'" target="_blank">Delete</a>
+        /
+        <a href="'.$edit.'" target="_blank">Edit</a>
+  </tr>
+  ';}
+  ?>
+    </table>
+</body>
+</html>
+<!-- 
+$del = 'http://localhost/check.file/delete.php?uid='.$row->topic_id;
+    echo'
+    <td>'.$row->topic_id.'</td>
+    <td>'.$row->datetime.'</td>
+    <td>'.$row->topic_name.'</td>
+    <td>'.$row->preview_link.'</td>
+    <td>
+    <a href="'.$del.'"  target="_blank">Delete</a>
+    </tr>
+    ';} -->
