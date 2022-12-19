f16657b202cb9f2ea2b7a631d428ed6fd07e8a89
unknown <em.satyam@gmail.com>
2022-12-19 23:43:43 +0530

    Update file Modified #191022

--- update.php
+++ update.php
@@ -1,5 +1,6 @@
 <?php
  
+
  $datetime = $_GET["date"];
  $topic_name = $_GET["topic"];
  $preview_link = $_GET["preview_link"];
@@ -7,7 +8,11 @@
 
 require_once("db/connection.php");
 
-$main = $connection->prepare("INSERT INTO `officework`( datetime, topic_name, preview_link) VALUE ( :db_datetime, :db_topic_name, :db_preview_link)")  ;
+$main = $connection->prepare("UPDATE `officework` SET( datetime, topic_name, preview_link) VALUE ( :db_datetime, :db_topic_name, :db_preview_link)")  ;
+$main = $connection->prepare("UPDATE `officework` SET first_name = '{$first_name}', last_name = '{$last_name}', dob = '{$dob}', email = '{$email}', mobile_no = '{$mobile_no}', country = '{$country}' WHERE id = '{$student_id}'");
+
+
+
 
 $main->bindParam(':db_datetime', $datetime);
 $main->bindParam(':db_topic_name', $topic_name);
