b5fc876bb94623b181a33f6900c831f71ecc25f6
unknown <em.satyam@gmail.com>
2022-12-23 23:17:40 +0530

    Initial Commit For Connection File

---
+++ database/delete.php
@@ -0,0 +1,18 @@
+<?php
+$id = $_GET["uid"];
+
+try{
+require_once(__DIR__."/db/connection.php");
+
+$sql_query = ("DELETE FROM `officework` WHERE topic_id = {$id} ");
+
+
+$connection->exec($sql_query);
+
+echo "Your Record has Deleted Succesfully";
+
+}catch(PDOException $ex_msg){
+	echo "Error: ". $ex_msg->getMessage();
+}
+
+?>
